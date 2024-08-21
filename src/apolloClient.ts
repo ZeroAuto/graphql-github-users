7b4c613fd838f5dc05ba0142bfe808bd68939b9e
Mike James <mikejames386@gmail.com>
2024-08-20 21:03:59 -0400

    add a comment in apollo client file

--- src/apolloClient.ts
+++ src/apolloClient.ts
@@ -6,6 +6,10 @@ const httpLink = new HttpLink({
 });
 
 const authLink = setContext(() => {
+  // under normal circumstances I would never put an access token into
+  // an environment variable but since there is no plan to ever deploy
+  // this code (as far as I know) I am doing it for convenience, happy
+  // to discuss more secure token methods during the interview process
   const token = process.env.REACT_APP_ACCESS_TOKEN;
   return {
     headers: {
